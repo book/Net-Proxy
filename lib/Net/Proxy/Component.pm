ca090b562d0ef0f9b714f50c0d5aba249b35c14e
Philippe Bruhat (BooK) <book@cpan.org>
2007-09-30 08:49:16 +0000

    New component class
    git-svn-id: svn://svn.mongueurs.net/Net-Proxy/branches/rewrite-2007@302 c0b37de7-d405-0410-92e2-9aae08bfc7c9

---
+++ lib/Net/Proxy/Component.pm
@@ -0,0 +1,70 @@
+package Net::Proxy::Component;
+
+use strict;
+use warnings;
+
+use Net::Proxy::Node;
+our @ISA = qw( Net::Proxy::Node );
+
+#
+# CLASS METHODS
+#
+sub build_factory_class {
+    my ($class) = @_;
+    my ($component) = $class =~ m/^Net::Proxy::Component::(.*)$/;
+
+    # eval the factory building code
+    eval << "FACTORY";
+    package Net::Proxy::ComponentFactory::$component;
+    use Net::Proxy::ComponentFactory;
+    our \@ISA = qw( Net::Proxy::ComponentFactory );
+FACTORY
+
+    return;
+}
+
+sub new {
+    my ($class, $args) = @_;
+
+    my $self = bless { %{ $args || {} } }, $class;
+    return $self;
+}
+
+#
+# INSTANCE METHODS
+#
+sub process {
+    my ($self, $message, $direction) = @_;
+
+    my $action = $message->type();
+    if ( $self->can($action) ) {
+        $message = $self->$action( $message, $direction );
+    }
+
+    # ABORT
+    return if $message->type() eq 'ABORT';
+
+    # pass the message on to the next node
+    my $next = $self->next( $direction );
+    $next->process( $message, $self )
+        if defined $next && $next->isa('Net::Proxy::Node');
+
+    return;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Net::Proxy::Component - A component in a Net::Proxy chain
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+=cut
+
