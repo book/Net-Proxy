f33124a2594c6a1c079b7aa28698a94a0234fce9
Philippe Bruhat (BooK) <book@cpan.org>
2007-10-23 22:25:38 +0000

    add the process method to the node mixin class
    git-svn-id: svn://svn.mongueurs.net/Net-Proxy/branches/rewrite-2007@346 c0b37de7-d405-0410-92e2-9aae08bfc7c9

--- lib/Net/Proxy/Node.pm
+++ lib/Net/Proxy/Node.pm
@@ -27,6 +27,27 @@ sub set_next {
     return $self->{"_next_$direction"} = $next;
 }
 
+sub process {
+    my ( $self, $messages, $from, $direction ) = @_;
+
+    push @$messages, undef;    # sentinel
+
+    while ( my $message = shift @$messages ) {
+
+        # actually process the message
+        my $action = $message->type();
+        if ( $self->can($action) ) {
+            $message = $self->$action( $message, $from, $direction );
+            push @$messages, $message if $message;
+        }
+
+        # pass the message to the next node
+        my $next = $self->next($direction);
+        $next->process( $message, $self, $direction )
+            if blessed $next && $next->isa('Net::Proxy::Node');
+    }
+}
+
 1;
 
 __END__
@@ -75,6 +96,15 @@ the chain, or if there is no chain in that direction.
 
 Note that the last object in a chain may not be a C<Net::Proxy::Node>
 object. It must, however, be a blessed object.
+
+=item process( $messages, $from, $direction )
+
+Process a message list.
+
+Each message is processed by the appropriate method (if any),
+and passed to the next node in the chain.
+
+=back
 
 =head1 AUTHOR
 
