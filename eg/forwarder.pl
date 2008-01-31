4597f1bdfe5bd4de1df63982fb98ffeb4e13e260
Philippe Bruhat (BooK) <book@cpan.org>
2008-01-31 09:17:24 +0000

    start benchmarking at the first connection, avoid calling the hook more than once
    git-svn-id: svn://svn.mongueurs.net/Net-Proxy/trunk@421 c0b37de7-d405-0410-92e2-9aae08bfc7c9

--- eg/forwarder.pl
+++ eg/forwarder.pl
@@ -1,3 +1,4 @@
+#!/usr/bin/env perl
 use strict;
 use warnings;
 use Net::Proxy;
@@ -8,7 +9,7 @@ use Pod::Usage;
 my %conf = (
     proxy       => 10000,
     server      => 10001,
-    connections => 0,
+    connections => 1,
 );
 GetOptions( \%conf, 'help', 'manual', 'verbose+', map {"$_=s"} keys %conf )
     or pod2usage(2);
@@ -16,7 +17,7 @@ pod2usage(1) if $conf{help};
 pod2usage( -verbose => 2 ) if $conf{manual};
 
 # mark the beginning of the benchmark/profiling
-my $t0 = Benchmark->new;
+my $t0;
 
 for my $item (qw( proxy server )) {
     $conf{$item} = ":$conf{$item}" if $conf{$item} !~ /:/;
@@ -30,6 +31,10 @@ my $proxy = Net::Proxy->new(
             host    => $conf{proxy}[0] || 'localhost',
             port    => $conf{proxy}[1],
             timeout => 1,
+            hook    => sub {
+                $t0 ||= Benchmark->new;
+                Net::Proxy->set_callback( $_[1] );    # remove ourselves now
+                }
         },
         out => {
             type => 'tcp',
@@ -62,10 +67,10 @@ forwarder.pl [ options ]
     --help                    Display this help
     --manual                  Longer manpage
     --verbose                 Increase verbosity (may be used several times)
-    --proxy  [<host>:]<port>  Local port to be forwarded
-    --server [<host>:]<port>  Destination server
+    --proxy  [<host>:]<port>  Local port to be forwarded (default: 10000)
+    --server [<host>:]<port>  Destination server         (default: 10001)
     --connections <n>         Maximum number of connection to accept
-                              (0 means never stop)
+                              (0 means never stop, default is 1)
 
 =head1 DESCRIPTION
 
